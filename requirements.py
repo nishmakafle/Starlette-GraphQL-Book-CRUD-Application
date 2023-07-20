a7629010e7d550826957af40632c8dcf83ecccd9
Nishma <nishma.kafle225@gmail.com>
2023-07-20 21:17:17 +0545

    initial commit

---
+++ requirements.py
@@ -0,0 +1,16 @@
+anyio==3.7.1
+click==8.1.6
+exceptiongroup==1.1.2
+graphql-core==3.2.3
+greenlet==2.0.2
+h11==0.14.0
+idna==3.4
+psycopg2-binary==2.9.6
+python-dateutil==2.8.2
+six==1.16.0
+sniffio==1.3.0
+SQLAlchemy==2.0.19
+starlette==0.30.0
+strawberry-graphql==0.195.2
+typing_extensions==4.7.1
+uvicorn==0.23.1
---
+++ schema.py
@@ -0,0 +1,34 @@
+import typing
+from datetime import date
+
+import strawberry
+
+
+@strawberry.type
+class Category:
+    id:strawberry.ID
+    title:str
+
+
+@strawberry.type
+class TagSchema:
+    id:strawberry.ID
+    name:str
+
+
+@strawberry.type
+class Book:
+    id:strawberry.ID
+    name:str
+    author:str
+    description:str
+    category:Category
+    quantity:int
+    tag:list[TagSchema]
+
+
+@strawberry.type
+class CategoryDetail:
+    id:strawberry.ID
+    title:str
+    book:Book
